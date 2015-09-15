81559e6f0189b7f775423953952e78d31c08d633
ronco <ronco@costite.com>
2015-09-14 20:41:40 -0600

    preliminary server

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
+global.env = process.env.NODE_ENV || 'development';
+
 var express = require('express');
 var app = express();
+var redisConfig = {
+  host: 'localhost',
+  port: 6379,
+  database: 0
+};
+
+if (env === 'production') {
+  redisConfig = {
+
+  };
+}
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
+var nodeEmberCliDeployRedis = require('node-ember-cli-deploy-redis');
+app.use('/*', nodeEmberCliDeployRedis('pe-blog', redisConfig));
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
